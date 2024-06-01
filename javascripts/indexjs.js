f6e042600703e41379f80c8843c531f8f6c3a511
jyech <jyecharlettmartillano@gmail.com>
2024-06-01 22:15:55 +0800

    Functioning Product Cards

---
+++ javascripts/indexjs.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".custom-carousel").owlCarousel({
+      autoWidth: true,
+      loop: true
+    });
+
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
+
+  
